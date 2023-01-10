098620f09ba376fe3ecc8a498a86d4ce3be3e28f
Alejandro Saldarriaga <alejandroasp@gmail.com>
2023-01-10 12:25:42 -0500

    bugfix - update next.config to load correctly

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = {
-  /* config options here */
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
