66ff75834a0fdcf1f14e17b61d82edcea6613657
filgaia <30418424+filgaia@users.noreply.github.com>
2023-01-11 16:00:20 +0000

    Deploying to gh-pages from @ filgaia/elden-ring-progression@7e9c17c57ab08f227c4337eff57540e2443a9e6a 🚀

---
+++ _next/static/lBpNxn-XczVY5PFYIdAcZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/817-626558e9f9d823a3.js","static/chunks/pages/index-f04093f6f43fd7f9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
